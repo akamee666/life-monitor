b2cbacd77ff41a92023735c9e57d501758ecc82c
moraeskkj <mooraesz123@gmail.com>
2024-07-08 15:22:01 +0000

    Created readme, capture events implemented.
    * At this point we are able to capture and increase the amount of times
      an event occurs using rdev crate, but only for keys, need to implement and calculate
      how much the mouse have moved cause things go wrong when you use the
      same approach as for keys.

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,55 @@
 // that i use the outputs of my setup, my keyboard, mouse and also monitor what i'm doing daily.
 // The whole point of this is to create some graphs in an personal blog as i explained in README.md
 use rdev::{listen, Event};
+static mut LEFT_CLICKS: i64 = 0;
+static mut MIDDLE_CLICKS: i64 = 0;
+static mut RIGHT_CLICKS: i64 = 0;
+static mut KEY_PRESSED: i64 = 0;
+static mut MOUSE_MOV: i64 = 0;
+static mut LAST_X_PX: i64 = 0;
+static mut LAST_Y_PX: i64 = 0;
 
-// pub struct Event {
-//     pub time: SystemTime,
-//     pub name: Option<String>,
-//     pub event_type: EventType,
-// }
-//
-//
 fn callback(event: Event) {
-    println!("My callback {:?}", event);
-    match event.name {
-        Some(string) => println!("User wrote {:?}", string),
-        None => (),
+    match event.event_type {
+        rdev::EventType::ButtonPress(button) => {
+            // println!("My callback {:?}", button);
+
+            match button {
+                rdev::Button::Left => unsafe {
+                    LEFT_CLICKS += 1;
+                    println!("amount of left clicks: {}", LEFT_CLICKS);
+                },
+                rdev::Button::Right => unsafe {
+                    RIGHT_CLICKS += 1;
+                    println!("amount of right clicks: {}", RIGHT_CLICKS);
+                },
+                rdev::Button::Middle => unsafe {
+                    MIDDLE_CLICKS += 1;
+                    println!("amount of middle clicks: {}", MIDDLE_CLICKS);
+                },
+                _ => {}
+            }
+        }
+
+        rdev::EventType::KeyPress(_) => unsafe {
+            KEY_PRESSED += 1;
+            println!("amount of key pressed: {}", KEY_PRESSED);
+        },
+
+        rdev::EventType::MouseMove { x, y } => unsafe {
+            // you cannot just increase cause sometimes the amount o pixel between events jumps
+            // like 5 or more px, depends on the speed you are moving your mouse.
+            // maybe keep track of last cordinates and calculate the difference based on that, here
+            // there is a link to help you: https://stackoverflow.com/questions/68288183/detect-mousemove-of-x-pixels
+
+            println!("x: {}", x);
+            println!("y: {}", y);
+            println!("MOUSE MOV: {}px", MOUSE_MOV);
+
+            // 1000 pixel (X)	26.4583333333 cm
+        },
+
+        _ => {}
     }
 }
 
