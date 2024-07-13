16a4aaa07596964f41f4ab8edcf7f191b576e3f2
アカメ <mooraesz123@gmail.com>
2024-07-13 12:57:17 +0000

    The code will need to refactor soon, also has bugs that i need to fix
    * Moved log functions to log.rs
    * Struggling to get the right time of a process

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 // The main purpose of this project is to create a program that will monitor the amount of times
 // that i use the outputs of my setup, my keyboard, mouse and also monitor what i'm doing daily.
 // The whole point of this is to create some graphs in an personal blog as i explained in README.md
-use chrono::{DateTime, Timelike, Utc};
-use core::panic;
 use rdev::{listen, Event};
-use std::fs::*;
-use std::io::*;
 use std::thread;
 use std::time::Duration;
 use sysinfo::*;
+
 static mut LEFT_CLICKS: i64 = 0;
 static mut MIDDLE_CLICKS: i64 = 0;
 static mut RIGHT_CLICKS: i64 = 0;
@@ -18,6 +15,7 @@ static mut LAST_X_PX: f64 = 0.0;
 static mut LAST_Y_PX: f64 = 0.0;
 
 fn callback(event: Event) {
+    // each time an event occurs, increment the global var by one.
     match event.event_type {
         rdev::EventType::ButtonPress(button) => match button {
             rdev::Button::Left => unsafe {
@@ -41,6 +39,7 @@ fn callback(event: Event) {
         },
 
         rdev::EventType::MouseMove { x, y } => unsafe {
+            // https://stackoverflow.com/questions/68288183/detect-mousemove-of-x-pixels
             if LAST_X_PX != 0.0 {
                 let power_x: f64 = (LAST_Y_PX - y).powf(2.0);
                 let power_y: f64 = (LAST_X_PX - x).powf(2.0);
@@ -50,114 +49,108 @@ fn callback(event: Event) {
 
             LAST_X_PX = x;
             LAST_Y_PX = y;
-            //println!("amount of mouse movement: {}cm", (MOUSE_MOV * 0.026) as i64);
         },
 
         _ => {}
     }
 }
 
-fn log(file: &mut File, s: String) {
-    #[cfg(debug_assertions)]
-    {
-        print!("{}", s);
-    }
-
-    match file.write(s.as_bytes()) {
-        Err(e) => {
-            println!("Couldn't write to log file: {}", e)
-        }
-        _ => {}
-    }
-
-    match file.flush() {
-        Err(e) => {
-            println!("Couldn't flush log file: {}", e)
-        }
-        _ => {}
-    }
-}
-
-fn create_log_file() -> File {
-    let now: DateTime<Utc> = Utc::now();
-    let filename = format!(
-        "log-{}-{:02}+{:02}+{:02}.log",
-        now.date_naive(),
-        now.hour(),
-        now.minute(),
-        now.second()
-    );
-
-    let logfile = {
-        match OpenOptions::new().write(true).create(true).open(&filename) {
-            Ok(f) => f,
-
-            Err(e) => {
-                panic!("Could not create the log file {}", e)
-            }
-        }
-    };
-    logfile
-}
-
+static mut TIME_CODING: u64 = 0;
 fn find_running_processes() {
-    // am i going to have categories for apps running?
-    // gaming, chatting, procastination, coding :)
+    let mut time_alacritty: u64 = 0;
     loop {
-        thread::sleep(Duration::from_millis(10));
+        thread::sleep(Duration::from_millis(100));
         let mut sys = System::new_all();
 
         // Update information of your struct
         sys.refresh_all();
 
-        // Display processes ID, name na disk usage:
+        // The logic here is valid but only if i have one opened window at the moment.
         for (pid, process) in sys.processes() {
             if process.status() == ProcessStatus::Run && process.name() != "life-agent-for-" {
-                println!(
-                    "[{pid}] name: {:?} memory: {:?} run_time: {:?} status: {:?}",
-                    process.name(),
-                    process.memory(),
-                    process.run_time(),
-                    process.status()
-                );
+                match process.name() {
+                    // coding.
+                    "nvim" => unsafe {
+                        // Solution: ? # Changing the library to have a function like set_run_time
+                        // would work in this case, i think.
+                        //
+                        // System -> inner: SystemInner -> processes: HashMap<Pid,Process> , global_cpu: Cpu
+                        // The set_run_time function should be in process.rs -> Impl ProcessInner
+                        //
+                        // 1. First time opening the program.
+                        // 2. Another windows is already running.
+                        // 3. The program had been reopened and the timer go back to zero.
+                        // ### UNRELATED
+                        // common.rs from sysinfo library can help you with a idea to increment the
+                        // right kind of time
+                        if process.run_time() < TIME_CODING {
+                            TIME_CODING += process.run_time();
+
+                            println!("increment the runtime");
+                            println!(
+                                "name: [{:?}], pid: [{:?}], time_coding: {:?}s, current_time: {:?}s",
+                                process.name(),
+                                process.pid(),
+                                TIME_CODING,
+                                process.run_time()
+                            );
+
+                            break;
+                        } else {
+                            TIME_CODING = process.run_time();
+                            println!("get the run_time");
+                            println!(
+                                "name: [{:?}], pid: [{:?}], time_coding: {:?}s, current_time: {:?}s",
+                                process.name(),
+                                process.pid(),
+                                TIME_CODING,
+                                process.run_time()
+                            );
+                        }
+                    },
+
+                    // just to clippy dont mess with my code.
+                    "123" => unsafe {
+                        // if this is true than the program was closed and reopened.
+                        if process.run_time() < time_alacritty {
+                            time_alacritty = process.run_time();
+                            TIME_CODING += process.run_time() + time_alacritty;
+
+                            println!(
+                             "name: [{:?}] pid: {:?} TIME_CODING: {:?}, time_alacritty: {:?}, start: {:?}",
+                             process.name(),
+                             process.pid(),
+                             TIME_CODING,
+                             time_alacritty,
+                             process.start_time(),
+                         );
+                            break;
+                        }
+
+                        TIME_CODING += process.run_time() - time_alacritty;
+                        time_alacritty = process.run_time();
+
+                        println!(
+                             "name: [{:?}] pid: {:?} TIME_CODING: {:?}, time_alacritty: {:?}, start: {:?}",
+                             process.name(),
+                             process.pid(),
+                             TIME_CODING,
+                             time_alacritty,
+                             process.start_time(),
+                         );
+                    },
+                    _ => {}
+                }
             }
         }
     }
 }
 
 fn main() {
-    //let logfile_fd = create_log_file();
+    //https://rust-lang.github.io/async-book/01_getting_started/02_why_async.html
     find_running_processes();
+
     if let Err(error) = listen(callback) {
         println!("Error: {:?}", error)
     }
 }
-
-//
-// fn log_header(file: &mut File) {
-//     let os_info = {
-//         let info = os_info::get();
-//         format!(
-//             "OS: type: {}\nVersion: {}\n",
-//             info.os_type(),
-//             info.version()
-//         )
-//     };
-//
-//     log(file, os_info);
-//     let os_hostname = hostname::get().unwrap().into_string().unwrap();
-//     println!("hostname: {:?}", os_hostname);
-//     log(file, os_hostname);
-// }
-//
-//fn run_spy(fd: &mut File) {
-
-//GetUserDefaultLocaleName
-//GetForegroundWindow
-//GetwindowsthereadProcessId
-//OpenProcess
-//GetProcessImageFileNameW
-//GetWindowTextLengthW
-//GetWindowTextW
-//GetAsyncKeyState
-//}
