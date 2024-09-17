ba9485e05a1e504f122b45a058d7471b37e9ebad
akamee666 <mooraesz123@gmail.com>
2024-09-17 11:53:11 -0300

    Adding source for x11 in main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ mod win;
 async fn main() {
     // TODO:
     // 1# Add linux support again.
+    // https://www.reddit.com/r/rust/comments/f7yrle/comment/hsgz16m/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
     // 2# Ask for arguments to setup database.
     // 3# Fix mouse accuracy.
     // 4# Change data struct when getting processes to handle name of the window as well.
