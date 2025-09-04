5bc4c8606fc55b9f5712b55b012afef80058026d
アカメ <mooraesz123@gmail.com>
2025-09-04 16:00:30 -0300

    refactor: changing fn name for better readability
    * changing the find_path fn to instead of returning the path of the
    database file, it will return the directory where the program store
    files; database, log and lock files. caller then must push the file name
    after the call

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-//#![windows_subsystem = "windows"]
-// used to close the terminal and create a gui with no window in Windows.
-
 #[cfg(target_os = "windows")]
 use crate::platform::windows::common::*;
 
+#[cfg(target_os = "windows")]
+use crate::platform::windows::process;
+
+#[cfg(target_os = "windows")]
+use crate::platform::windows::systray;
+
 #[cfg(target_os = "linux")]
 use crate::platform::linux::common::*;
+
+#[cfg(target_os = "linux")]
+use crate::platform::linux::process;
 
 use crate::storage::backend::*;
 use crate::utils::args::Cli;
@@ -13,9 +19,6 @@ use crate::utils::lock::*;
 use crate::utils::logger;
 
 use clap::Parser;
-
-use chrono::Local;
-use chrono::NaiveDate;
 
 use tokio::task::JoinSet;
 use tracing::*;
@@ -27,41 +30,35 @@ mod storage;
 mod utils;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut args = Cli::parse();
     logger::init(args.debug);
 
     // if we receive one of these two flags we call the function and it will enable or disable the
     // startup depending on the enable value.
     if args.enable_startup || args.disable_startup {
-        match configure_startup(&args) {
-            Ok(_) => {
-                info!(
-                    "Startup {} successfully, the program will end now. Start it again without the start up flag to run normally.",
-                    if args.enable_startup {
-                        "enabled"
-                    } else {
-                        "disabled"
-                    }
-                );
-                return;
-            }
-            Err(e) => {
-                error!("Failed to configure startup: {}", e);
-                return;
-            }
-        }
+        let state = if args.enable_startup {
+            "enabled"
+        } else {
+            "disabled"
+        };
+
+        configure_startup(&args).inspect_err(|_| {
+            error!("Failed to {} startup", state);
+        })?;
+
+        info!("Startup {}d successfully, the program will end now. Start it again without the start up flag to run normally.",state);
+        return Ok(());
     }
 
-    let today = NaiveDate::from_ymd_opt(2024, 1, 1).unwrap();
-    println!("today from_ymd_opt: {today}");
-    let today = Local::now().date_naive();
-    println!("today date_naive: {today}");
-
-    ensure_single_instance().unwrap_or_else(|err| {
+    if let Err(err) = ensure_single_instance() {
+        if err.kind() == std::io::ErrorKind::AlreadyExists {
+            warn!("Already have one instance of life-monitor running!");
+            return Ok(());
+        }
         error!("Failed to ensure single instance when starting application");
         panic!("{err}");
-    });
+    };
 
     debug!(
         "Lock acquired. Running application with PID {}",
@@ -75,43 +72,41 @@ async fn main() {
 
     // We choose the API backend if user provide a path of the config with remote flag
     let storage_backend: StorageBackend = if let Some(ref config_path) = args.remote {
-        match ApiStore::new(config_path) {
-            Ok(api) => StorageBackend::Api(api),
-            Err(e) => {
-                error!("Failed to initialize API backend due to {e}.");
-                panic!();
-            }
-        }
+        let api = ApiStore::new(config_path).unwrap_or_else(|err| {
+            error!("Failed to start API backend");
+            panic!("Fatal error: {err}");
+        });
+        StorageBackend::Api(api)
     } else {
-        let db = match LocalDbStore::new(args.gran, args.clear) {
-            Ok(db) => db,
-            Err(e) => {
-                error!("Failed to initialize SQLite backend due to {e}.");
-                panic!();
-            }
-        };
+        let db = LocalDbStore::new(args.gran, args.clear).unwrap_or_else(|err| {
+            error!("Failed to start SQLite backend");
+            panic!("Fatal error: {err}");
+        });
         StorageBackend::Local(db)
     };
 
     run(args, storage_backend).await;
+    Ok(())
 }
 
-#[cfg(target_os = "linux")]
 async fn run(args: Cli, backend: StorageBackend) {
-    use crate::keylogger;
-    use crate::platform::linux::process;
-
     let backend2 = backend.clone();
 
     // https://docs.rs/tokio/latest/tokio/task/struct.JoinSet.html
     let mut set = JoinSet::new();
 
     if !args.no_keys {
+        // TODO: DOesn't working in wayland bc rdev doesn't support it
         set.spawn(keylogger::init(args.dpi, args.interval, backend));
     }
 
     if !args.no_window {
         set.spawn(process::init(args.interval, backend2));
+    }
+
+    #[cfg(target_os = "windows")]
+    if !args.no_systray {
+        set.spawn(systray::init());
     }
 
     // Need to wait the tasks finish, which they should not if everything is okay and i'm not dumb.
@@ -119,51 +114,11 @@ async fn run(args: Cli, backend: StorageBackend) {
     // entire program.
     while let Some(res) = set.join_next().await {
         match res {
-            // That should not occur.
-            Ok(_) => error!("A task has unexpectedly finished"),
-            // panicked!
-            Err(e) => {
-                error!("A task has panicked: {}", e);
-                panic!()
-            }
-        }
-    }
-}
-
-#[cfg(target_os = "windows")]
-async fn run(args: Cli, backend: StorageBackend) {
-    use crate::keylogger;
-    use crate::platform::windows::process;
-    use crate::platform::windows::systray;
-
-    let backend2 = backend.clone();
-
-    // https://docs.rs/tokio/latest/tokio/task/struct.JoinSet.html
-    let mut set = JoinSet::new();
-
-    if !args.no_keys {
-        set.spawn(keylogger::init(args.dpi, args.interval, backend));
-    }
-
-    if !args.no_window {
-        set.spawn(process::init(args.interval, backend2));
-    }
-
-    if !args.no_systray {
-        set.spawn(systray::init());
-    }
-
-    // Need to wait the tasks finish, which they should not if there is no error.
-    // Without wait for them, run function will finish and all values will be droped, finishing the
-    // entire program.
-    while let Some(res) = set.join_next().await {
-        match res {
-            // That should not occur.
-            Ok(_) => error!("A task has unexpectedly finished"),
-            // panicked!
-            Err(e) => {
-                error!("A task has panicked: {}", e);
-                panic!()
+            // That should not occur, i think?.
+            Ok(_) => warn!("task has unexpectedly finished"),
+            Err(err) => {
+                error!("A task has panicked");
+                panic!("fatal error in task: {err}")
             }
         }
     }
